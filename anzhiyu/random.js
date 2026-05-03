53845a13f62a260e3dfe65391d259a46d8caac62
lishanganshijie-hue <lishanganshijie-hue@users.noreply.github.com>
2026-05-03 19:04:07 +0000

    Auto deploy from Qexo 45ab9b7666193846501a5cf2c63a8716b9caa57a

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2026/05/03/这是测试内容/"];function toRandomPost(){
+var posts=["2026/05/03/这是测试内容/","2026/05/03/厉害厉害/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
